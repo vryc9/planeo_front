01834a309e2472b4186451f5202f01cf35a423c5
Enzo Volpato <enzo@MacBook-Air-de-Enzo.local>
2025-12-30 00:06:43 +0100

    feat : add calendar

--- src/app/feature/calendar/calendar-component.ts
+++ src/app/feature/calendar/calendar-component.ts
@@ -1,11 +1,85 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg, EventApi, EventClickArg } from '@fullcalendar/core/index.js';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import { createEventId, INITIAL_EVENTS } from './util/event-util';
 @Component({
   selector: 'app-calendar-component',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './calendar-component.html',
   styleUrl: './calendar-component.css',
 })
+
 export class CalendarComponent {
+  calendarVisible = signal(true);
+  calendarOptions = signal<CalendarOptions>({
+    height : '100%',
+    expandRows: true,
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    initialView: 'dayGridMonth',
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+        /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  });
+currentEvents = signal<EventApi[]>([]);
+
+   constructor(private changeDetector: ChangeDetectorRef) {
+  }
+
+
+  handleCalendarToggle() {
+    this.calendarVisible.update((bool) => !bool);
+  }
+
+  handleWeekendsToggle() {
+    this.calendarOptions.update((options) => ({
+      ...options,
+      weekends: !options.weekends,
+    }));
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents.set(events);
+    this.changeDetector.detectChanges(); // workaround for pressionChangedAfterItHasBeenCheckedError
+  }
 
 }
