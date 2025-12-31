aa05731013e3fb75493bc7c08fc651be10aab5f3
Enzo Volpato <enzo@MacBook-Air-de-Enzo.local>
2025-12-31 02:50:44 +0100

    refactor: delete useless readonly

--- src/app/feature/dashboard/dashboard-container-component.ts
+++ src/app/feature/dashboard/dashboard-container-component.ts
@@ -29,7 +29,7 @@ export class DashboardContainerComponent {
     [DashboardViewEnum.INVESTMENT]: InvestmentComponent
   };
 
-  readonly currentComponent = () =>
+  currentComponent = () =>
     this.components[this.store.currentView()];
 
 }
